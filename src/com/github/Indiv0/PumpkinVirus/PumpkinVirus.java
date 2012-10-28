001893c72252ad2f142c0ab300827d5bd4bed19f
Indiv0 <indiv0soft@gmail.com>
2012-10-28 18:49:45 -0400

    Implemented PluginMetrics

--- src/com/github/Indiv0/PumpkinVirus/PumpkinVirus.java
+++ src/com/github/Indiv0/PumpkinVirus/PumpkinVirus.java
@@ -1,7 +1,30 @@
 package com.github.Indiv0.PumpkinVirus;
 
+import java.io.IOException;
+import java.util.logging.Level;
+
 import org.bukkit.plugin.java.JavaPlugin;
+import org.mcstats.MetricsLite;
 
 public class PumpkinVirus extends JavaPlugin {
+    @Override
+    public void onLoad() {
+        // Enable PluginMetrics.
+        enableMetrics();
+    }
 
+    private void enableMetrics()
+    {
+        try {
+            MetricsLite metrics = new MetricsLite(this);
+            metrics.start();
+        } catch (IOException ex) {
+            logException(ex, Level.WARNING, "An error occured while attempting to connect to PluginMetrics.");
+        }
+    }
+
+    public void logException(Exception ex, Level level, String message) {
+        ex.printStackTrace(System.out);
+        getLogger().log(level, message);
+    }
 }
