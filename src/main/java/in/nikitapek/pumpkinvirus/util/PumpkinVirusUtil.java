6a76d77246ee55ffecacee336d34c29d3a2c8cf0
Nikita Pekin <nikitapekin@gmail.com>
2013-05-25 08:57:13 -0400

    Forgot to add Util class

---
+++ src/main/java/in/nikitapek/pumpkinvirus/util/PumpkinVirusUtil.java
@@ -0,0 +1,9 @@
+package in.nikitapek.pumpkinvirus.util;
+
+import java.util.Random;
+
+public final class PumpkinVirusUtil {
+    public static final Random RANDOM = new Random();
+
+    private PumpkinVirusUtil() {}
+}
