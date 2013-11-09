3f7759f5ed2058cd962838395ff481b31a0a4b03
Nikita Pekin <nikitapekin@gmail.com>
2013-11-09 15:16:38 -0500

    Add extra configuration values

--- src/main/java/in/nikitapek/pumpkinvirus/util/PumpkinVirusConfigurationContext.java
+++ src/main/java/in/nikitapek/pumpkinvirus/util/PumpkinVirusConfigurationContext.java
@@ -4,6 +4,8 @@ import com.amshulman.mbapi.MbapiPlugin;
 import com.amshulman.mbapi.util.ConfigurationContext;
 import com.amshulman.typesafety.TypeSafeSet;
 import com.amshulman.typesafety.impl.TypeSafeSetImpl;
+import org.bukkit.Bukkit;
+import org.bukkit.Material;
 
 import java.util.HashSet;
 import java.util.List;
@@ -11,6 +13,10 @@ import java.util.logging.Level;
 
 public final class PumpkinVirusConfigurationContext extends ConfigurationContext {
     public final int ticks;
+    public final boolean trackingPlayers;
+    public final int maxHeightOffGround;
+    public final Material virusBlockType;
+    public final Material antiVirusBlockType;
 
     public final TypeSafeSet<String> worlds;
 
@@ -23,6 +29,15 @@ public final class PumpkinVirusConfigurationContext extends ConfigurationContext
 
         // Retrieves the ticks between pumpkin spread attempts.
         ticks = plugin.getConfig().getInt("spreadSpeed", 5);
+        // Retrieves whether or not pumpkins should attempt to track and kill players.
+        trackingPlayers = plugin.getConfig().getBoolean("trackingPlayers", true);
+        // Retrieves the maximum height pumpkins are allowed to get off the ground.
+        maxHeightOffGround = plugin.getConfig().getInt("maxHeightOffGround", 5);
+
+        // Retrieves the block type to use for the virus.
+        virusBlockType = retrieveMaterialFromConfig("virusBlockType", Material.PUMPKIN);
+        // Retrieves the block type to use for the anti-virus.
+        antiVirusBlockType = retrieveMaterialFromConfig("antiVirusBlockType", Material.MELON);
 
         // Attempts to read the configurationSection containing the worlds in which pumpkins are allowed to spread.
         @SuppressWarnings("unchecked")
@@ -34,5 +49,27 @@ public final class PumpkinVirusConfigurationContext extends ConfigurationContext
                 worlds.add(world);
             }
         }
+    }
+
+    /**
+     * Attempts to retrieve a material from the plugin configuration.
+     * @param key the key for the material to be retrieved.
+     */
+    Material retrieveMaterialFromConfig(String key, Material defaultMaterial) {
+        String materialString = plugin.getConfig().getString(key);
+
+        if (materialString == null) {
+            Bukkit.getLogger().log(Level.WARNING, "Invalid value for key: " + key);
+            return defaultMaterial;
+        }
+
+        Material material = Material.getMaterial(materialString);
+
+        if (material == null) {
+            Bukkit.getLogger().log(Level.WARNING, "Invalid value for key: " + key);
+            return defaultMaterial;
+        }
+
+        return material;
     }
 }
